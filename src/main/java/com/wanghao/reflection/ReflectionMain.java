8fe89054b0df71e2aa53225deb6420fefedca875
wanghao2 <wanghao2@gomeholdings.com>
2018-01-13 17:25:00 +0800

    增加反射

--- src/main/java/com/wanghao/reflection/ReflectionMain.java
+++ src/main/java/com/wanghao/reflection/ReflectionMain.java
@@ -4,6 +4,7 @@ package com.wanghao.reflection;/**
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * 
@@ -83,7 +84,7 @@ public class ReflectionMain {
         }
 
 
-        Field fields2=clz3.getField("id");
+        Field fields2=clz3.getDeclaredField("id");
         stu= (Student) clz3.getConstructor().newInstance();
         stu= (Student) clz3.newInstance();
         System.out.println("-------通过名字获取成员变量--------------");
@@ -94,6 +95,37 @@ public class ReflectionMain {
 //http://blog.csdn.net/sinat_38259539/article/details/71799078
 
 
+        System.out.println("-----------------获取成员方法---------------------");
+
+        Method[] methods=clz3.getMethods();
+        System.out.println("获取所有的public method");
+        for(Method method:methods){
+            System.out.println(method);
+        }
+
+
+        System.out.println("获取所有的 All  method ");
+        
+        methods = clz3.getDeclaredMethods();
+        for(Method method:methods){
+            System.out.println(method);
+        }
+        
+        
+        
+        Method m=clz3.getMethod("show1",String.class);
+        m.invoke(stu,"hello");
+        
+        m=clz3.getMethod("show3",null);
+        m.invoke(stu);
+
+        System.out.println("----------获取私有的方法-----------");
+        
+        m=clz3.getDeclaredMethod("show4",int.class);
+        m.setAccessible(true);
+        m.invoke(stu,43);
+        
+        
     }
     
     
--- src/main/java/com/wanghao/reflection/Student.java
+++ src/main/java/com/wanghao/reflection/Student.java
@@ -59,6 +59,19 @@ public class Student {
     public void setSex(boolean sex) {
         this.sex = sex;
     }
-    
-    
+
+    public void show1(String s){
+        System.out.println("调用了：公有的，String参数的show1(): s = " + s);
+    }
+    protected void show2(){
+        System.out.println("调用了：受保护的，无参的show2()");
+    }
+    public  void show3(){
+        System.out.println("调用了：public，无参的show3()");
+    }
+    private String show4(int age){
+        System.out.println("调用了，私有的，并且有返回值的，int参数的show4(): age = " + age);
+        return "abcd";
+    }
+
 }
